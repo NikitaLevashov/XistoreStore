516833415aee6c27a73eb99055b73950847df21b
levashov_92 <levashov_92@outlook.com>
2021-01-27 11:06:16 +0300

    Add Migration

--- XistoreStore/Program.cs
+++ XistoreStore/Program.cs
@@ -9,6 +9,20 @@ using Microsoft.Extensions.Logging;
 
 namespace XistoreStore
 {
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        CreateHostBuilder(args).Build().Run();
+    //    }
+
+    //    public static IHostBuilder CreateHostBuilder(string[] args) =>
+    //        Host.CreateDefaultBuilder(args)
+    //            .ConfigureWebHostDefaults(webBuilder =>
+    //            {
+    //                webBuilder.UseStartup<Startup>().UseDefaultServiceProvider(options=>options.ValidateScopes=false).Build();
+    //            });
+    //}
     public class Program
     {
         public static void Main(string[] args)
--- XistoreStore/Startup.cs
+++ XistoreStore/Startup.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using XistoreStore.Models;
@@ -14,12 +16,17 @@ namespace XistoreStore
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+        public IConfiguration Configuration { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AplicationDbContext>(options => options.UseSqlServer
+            (Configuration["Data:XistoreStoreProducts:ConnectionString"]));
+            services.AddDbContext<AplicationDbContext>();
             services.AddMvc();
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddTransient<IProductRepository, EFProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
